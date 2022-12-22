22b3d2c8a035d93d7579f81bd86df6d93f0d1fd9
Christian <c.espinoza.code@gmail.com>
2022-12-21 20:10:23 -0500

    cors library installed

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import express from 'express';
 import { routerApi } from './routes';
 import { logErrors, errorHandler, boomErrorHandler } from './middleware/error.handler';
+import cors from 'cors';
 
 const app = express();
 
 const port = 3000;
 
+/* Recibir solicitudes en formato JSON */
 app.use(express.json());
+
+/* Configurar el acceso a la API desde cualquier dominio */
+//app.use(cors());
+
+/* Configurar el acceso a dominios personalizados */
+const whitelist = ['http://localhost:8080', 'http://localhost:5500', 'https://myapp.co']
+const options = {
+  origin: (origin: any, callback: any) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Acceso no permitido'))
+    }
+  }
+}
+app.use(cors(options))
 
 /* Routes assigned to application instance */
 routerApi(app);
